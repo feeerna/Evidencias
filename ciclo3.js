772da25f3c6c8081b2892073a550b399a2fa5118
feeerna <160561973+feeerna@users.noreply.github.com>
2024-03-30 19:10:27 -0500

    Delete ciclo3.js

--- ciclo3.js
+++
@@ -1,7 +0,0 @@
-Console.log("3. Numeros pares del 1 al 100")
-
-for (let i = 0; i < 101; i+=2) {
-    console.log(i);
-  }
-
-// si comienza desde le numero 1 no se pueden imprimir los nuemros pares
